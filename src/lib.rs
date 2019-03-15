fcd0cb313cada30d4a442a150634c628af309bb3
mzhong215 <Michelle_Zhong@comcast.com>
2019-03-15 16:04:27 -0400

    removed old comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,10 @@
-//! smbc is wrapper around `libsmbclient` from Samba project
-//!
-//! It provides basic `std::fs`-like API to access SMB/CIFS file shares
-
-/// Module with smbc's Result and Error coercions
-pub mod result;
+/// error handlers
+pub mod error;
 
 pub mod parser;
-/// Main API module (reexported later)
+/// API module
 pub mod smbc;
 
-pub use crate::{result::*, smbc::*};
+pub use crate::{error::*, smbc::*};
 
 pub use crate::parser::*;
